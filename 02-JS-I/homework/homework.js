03e7be957a2f8bf906d79ddc264227a68cd70df1
Oscar Herrera <oherrera84@outlook.com>
2022-10-13 13:20:56 -0500

    Add commit 029

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -192,7 +192,7 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
-  return 'Hola '+ nombre; + '!'
+  return 'Hola '+ nombre + '!';
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
@@ -232,7 +232,14 @@ function esVocal(letra){
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  var letra;
+  if (letra.length > 1) {
+    return "Dato incorrecto";
+  }
+  if(letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u"){
+    return "Es vocal"
+  }
+  return "Dato incorrecto"
 }
 
 
