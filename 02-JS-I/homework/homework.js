dcbc48c1e9156b79d2455f6cbc0d1f88adf42f40
Oscar Herrera <oherrera84@outlook.com>
2022-10-13 08:47:24 -0500

    Add commit 06

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -68,13 +68,12 @@ function sonIguales(x, y) {
   // Devuelve "true" si "x" e "y" son iguales
   // De lo contrario, devuelve "false"
   // Tu código:
-  const x = 5;
-  const y = 7;
+  var x = 5;
+  var y = 7;
   if (x === y) {
-    return 'true'
-  }else {
-    return 'false'
+    return true;
   }
+  return false;
 }
 
 function tienenMismaLongitud(str1, str2) {
