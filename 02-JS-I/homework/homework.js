afcfb466aba31c2b59987be9db53771ed2dea301
Oscar Herrera <oherrera84@outlook.com>
2022-10-13 09:05:39 -0500

    Add commit 09

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -7,7 +7,7 @@ const nuevaString = 'hola';
 const nuevoNum = 1984;
 
 // Crea una variable booleana:
-const nuevoBool = 'true';
+const nuevoBool = true;
 
 // Resuelve el siguiente problema matemático:
 const nuevaResta = 10 - 5 === 5;
