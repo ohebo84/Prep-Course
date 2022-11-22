c0f5a0f378fada7c04e2b0af919c4b80c669e8d3
Oscar Herrera <oherrera84@outlook.com>
2022-11-21 22:53:18 -0500

    Commit 11

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -126,7 +126,7 @@ function operadoresLogicos(num1, num2, num3) {
   }else if (num3 > num1 && num3 > num2) {
     num3 = num3 + 1;
     return num3;
-  }else if (num3 === 0 || num2 === 0 || num3 === 0) {
+  }else if (num1 === 0 || num2 === 0 || num3 === 0) {
     return "Error"
   }else {
     return false;
