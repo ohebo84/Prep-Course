6beb5c8c36b37a0a673b92580c70e1ab6979ca8d
Oscar Herrera <oherrera84@outlook.com>
2022-11-21 23:02:21 -0500

    Commit 11

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -153,6 +153,11 @@ function esVerdadero(valor){
   //Escribe una función que reciba un valor booleano y retorne “Soy verdadero” 
   //si su valor es true y “Soy falso” si su valor es false.
   //Escribe tu código aquí
+  if (valor === true) {
+    return "Soy verdadero";
+  }else {
+    return "Soy falso";
+  }
 
 }
 
