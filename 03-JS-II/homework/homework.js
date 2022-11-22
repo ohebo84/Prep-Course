612a742d19d07cf2703e826354fde07053a2668d
Oscar Herrera <oherrera84@outlook.com>
2022-11-21 23:29:30 -0500

    Commit 14

--- 03-JS-II/homework/homework.js
+++ 03-JS-II/homework/homework.js
@@ -164,13 +164,23 @@ function esVerdadero(valor){
 function tablaDelSeis(){
   //Escribe una función que muestre la tabla de multiplicar del 6 (del 0 al 60).
   //La función devuelve un array con los resultados de la tabla de multiplicar del 6 en orden creciente.
-  //Escribe tu código aquí   
+  //Escribe tu código aquí
+  let arrayTablaDel6 = []
+  for (let i = 0; i < 11; i++) {
+        arrayTablaDel6.push(6 * i)
+  }
+  return arrayTablaDel6;
   
 }
 
 function tieneTresDigitos(numero){
   //Leer un número entero y retornar true si tiene 3 dígitos. Caso contrario, retorna false.
   //Escribe tu código aquí
+  if (numero.length === 3) {
+    return true;
+  }else {
+    return false;
+  }
   
 }
 
