61c285d58c882eeab8f77aaeaf3c62edda52e0ec
Oscar Herrera <oherrera84@outlook.com>
2022-11-22 20:58:57 -0500

    Commit 03

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -68,6 +68,12 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  for(var i = 0; i < array.length; i++) {
+    if(array[i] === elemento) {
+      return true;
+    }
+  }
+  return false;
 }
 
 
@@ -75,6 +81,11 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var suma = 0;
+  for(var i = 0; i < numeros.length; i++) {
+    suma = suma + numeros[i];
+  }
+  return suma;
 }
 
 
