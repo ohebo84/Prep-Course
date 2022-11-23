007788e98878739c933ee790583d9f90ae2d2f80
Oscar Herrera <oherrera84@outlook.com>
2022-11-22 21:47:01 -0500

    Commit 12

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -218,6 +218,21 @@ function breakStatement(numero) {
   //devolver: "Se interrumpió la ejecución"
   //Pista: usá el statement 'break'
   // Tu código:
+  var array = [];
+  var suma = numero;
+  for(var i= 0; i<10; i++) {
+    suma = suma + 2;
+    if(suma === i) break;
+    else {
+      array.push(suma);
+    }
+  }
+  if(i < 10) {
+    return 'Se interrumpió la ejecución';
+  }
+  else {
+      return array;
+  }
 
 }
 
