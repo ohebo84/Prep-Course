eec245b9003129b8ab9b1ba6313db66b746cf490
Oscar Herrera <oherrera84@outlook.com>
2022-11-22 21:02:54 -0500

    Commit 04

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -115,7 +115,7 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
-  f(arguments.length < 1) return 0;
+  if(arguments.length < 1) return 0;
   var total = 1;
   for(var i = 0; i < arguments.length; i++) {
     total = total * arguments[i];
